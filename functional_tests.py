49b7140d5d10b18a5d72751594df85ce07df2cb4
Avy Faingezicht <avy@apple.com>
2016-03-14 00:55:41 -0700

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -10,14 +10,14 @@ class NewVisitorTest(unittest.TestCase):  #1
     def tearDown(self):  #3
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  #4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  #5
-        self.fail('Finish the test!')  #6
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
 
